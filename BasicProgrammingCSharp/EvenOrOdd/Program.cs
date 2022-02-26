2e35b68d3436e3ebe6b716f7d50c6cc3656ff302
Rashedul Islam <rashidkhan71011@gmail.com>
2022-02-27 01:47:33 +0600

    Even Or Odd Number Solution.

---
+++ BasicProgrammingCSharp/EvenOrOdd/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a Integer Number : ");
+int i = int.Parse(Console.ReadLine());
+
+if(i % 2 == 0)
+{
+    Console.WriteLine("Entered Number is an Even Number : ");
+}
+else
+{
+    Console.WriteLine("Entered Number is an Odd Number");
+}
